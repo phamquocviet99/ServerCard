4c939e63ab9a16936dfd8969e998a1d424224b8f
phamquocviet99 <43658936+phamquocviet99@users.noreply.github.com>
2023-08-07 07:18:07 +0700

    Update templateEmail.js

--- template/templateEmail.js
+++ template/templateEmail.js
@@ -4,8 +4,8 @@ export const templateEmail = (data) => {
   const domain = process.env.DOMAIN_SERVER;
 
   return ` <div>
- <p>Cảm ơn quý ${userData.gender === "male" ? "ông" : "bà"} ${
-    userData.fullName
+ <p>Cảm ơn quý ${data.gender === "male" ? "ông" : "bà"} ${
+    data.fullName
   } đã đăng kí tham gia sự kiện lễ ra mắt Sàn Hoa FMP !</p>
   <img src='${domain}/images/${data._id}/invitation.png' />
 </div>
